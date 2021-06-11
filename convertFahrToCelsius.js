7b98bd68683b51eaa03a190a72a5b549806fdda0
02awesome <81158572+02awesome@users.noreply.github.com>
2021-06-11 17:26:18 +0100

    Temperature Converter Complete

--- convertFahrToCelsius.js
+++ convertFahrToCelsius.js
@@ -1,3 +1,26 @@
-const convertFahrToCelsius = temp => {
-    
-}
+const convertFahrToCelsius = (tempFahr) => {
+  let tempCel;
+  if (typeof tempFahr !== "number" && typeof tempFahr !== "string") {
+    if (typeof tempFahr === "object") {
+      console.log(
+        `${Object.keys(tempFahr)}:${Object.values(
+          tempFahr
+        )} is not a valid number but a/an ${typeof tempFahr}.`
+      );
+    } else {
+      console.log(
+        `${tempFahr} is not a valid number but a/an ${typeof tempFahr}.`
+      );
+    }
+  } else {
+    if (isNaN(tempFahr)) {
+      console.log(`${tempFahr} cannot be converted to a number.`);
+    } else {
+      tempCel = (tempFahr - 32) * (5 / 9);
+      tempCel = tempCel.toFixed(4);
+      console.log(tempCel);
+    }
+  }
+};
+
+convertFahrToCelsius({ temp: 0 });
